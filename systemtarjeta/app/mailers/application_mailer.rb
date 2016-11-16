c018ff108d752223c0b9ab89c25c8e4103370a76
Sandra <cartas22c@gmail.com>
2016-11-17 00:46:11 +0100

    subida masiva

---
+++ systemtarjeta/app/mailers/application_mailer.rb
@@ -0,0 +1,12 @@
+class ApplicationMailer < ActionMailer::Base
+
+  default from: "systematarjeta@gmail.com"
+
+   def sample_email(user)
+     @user = user
+      
+      mail(to: @user.email, subject: 'Alta usuario')
+   end
+
+
+end
--- systemtarjeta/config/environments/development.rb
+++ systemtarjeta/config/environments/development.rb
@@ -14,7 +14,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,16 +41,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
- 
-
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name: "systematarjeta@gmail.com",
-  password: "221090ssaa"
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+ # SMTP settings for gmail
+ config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'systematarjeta@gmail.com',
+  :password             => '221090ssaa',
+  :authentication       => "plain",
+ :enable_starttls_auto => true
+ }
+
 
 end
